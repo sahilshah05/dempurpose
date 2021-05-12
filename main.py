6e2606632479917c3946662f5c88467092d34ff1
fotariyavivek <84018259+fotariyavivek@users.noreply.github.com>
2021-05-12 11:52:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-print("hello vivek")
+print("hello vivek from pycharm")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+print("hello vivek from GIT hub web chrome")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
